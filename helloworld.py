959d4fb815cdb32bf11e76bc91a3b4d4fbae384e
Ryan <26681130+MaironI@users.noreply.github.com>
2018-08-05 22:44:34 +0100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# Author: 'Mairon'
+# Project: Project World
+
+print('Hello World') # Print the string "Hello World"
+h = 'Hello' # Assign a string to 'h' variable.
+w = 'World' # Assign a string to 'w' variable.
+print(h, w) # Print h & w variable. Output = Hello World
